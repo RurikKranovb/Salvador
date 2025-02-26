4b307ad14c9c0ea4420bd7a7d7db1eecd529692f
bobrov.da <dmr.bobrov@yandex.ru>
2025-02-26 22:57:14 +0300

    Рефакторинг

---
+++ UI/Salvador.UI/Program.cs
@@ -0,0 +1,41 @@
+namespace Salvador.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var service = builder.Services;
+
+            // Add services to the container.
+            service.AddRazorPages();
+
+            service.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.UseRouting();
+
+            app.MapRazorPages();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run();
+        }
+    }
+}
