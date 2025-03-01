39c31546316850526ca8d45aecd96c79615766be
bobrov.da <dmr.bobrov@yandex.ru>
2025-03-01 12:20:11 +0300

    Заготовка контроллеров

---
+++ UI/Salvador.UI/Controllers/RecordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Salvador.UI.Controllers
+{
+    public class RecordController : Controller
+    {
+        public IActionResult Record()
+        {
+            return View();
+        }
+    }
+}
