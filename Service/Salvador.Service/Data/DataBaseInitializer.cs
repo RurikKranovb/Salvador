f4a8f4c110e0cf6367fcff74d09e23bf0b98e6fa
bobrov.da <dmr.bobrov@yandex.ru>
2025-03-01 14:18:44 +0300

    Initialize db

---
+++ Service/Salvador.Service/Data/DataBaseInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Salvador.DAL.Context;
+
+namespace Salvador.Service.Data
+{
+   public class DataBaseInitializer
+    {
+        private readonly DataBase _dataBase;
+
+        public DataBaseInitializer(DataBase dataBase)
+        {
+            _dataBase = dataBase;
+        }
+
+        public void Initializer() => InitializeAsync().Wait();
+
+        private async Task InitializeAsync()
+        {
+            var db = _dataBase.Database;
+
+            await db.MigrateAsync().ConfigureAwait(false);
+        }
+    }
+}
