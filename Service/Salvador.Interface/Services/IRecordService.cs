5b2f10e1da30efaf63e5240b836be4e5d3a6cc97
bobrov.da <dmr.bobrov@yandex.ru>
2025-03-01 14:40:12 +0300

    Хлам

--- Service/Salvador.Interface/Services/IRecordService.cs
+++ Service/Salvador.Interface/Services/IRecordService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Salvador.Interface.Services
+﻿namespace Salvador.Interface.Services
 {
     public interface IRecordService
     {
